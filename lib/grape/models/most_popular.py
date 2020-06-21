7f8c3943994cfea509a8ab5712bbe22cf81a31ba
h2-heo <h0h6h2h5@gmail.com>
2020-06-21 12:20:59 +0000

    Improve performance

--- lib/grape/models/most_popular.py
+++ lib/grape/models/most_popular.py
@@ -4,6 +4,7 @@ from lib.graph import SongNode
 from lib.graph import TagNode
 from ..utils import convert_to_ids
 from ..utils import get_most_common_keys
+from ..utils import remove_seen
 from ..utils import OrderedSet
 from .base import BaseModel
 
@@ -21,10 +22,8 @@ class MostPopular(BaseModel):
         tag_counts = Counter()
 
         for playlist in tqdm(playlists, "Fitting MostPopular model"):
-            songs = self._graph.get_nodes(
-                SongNode, playlist['songs'], ignore_none=True)
-            tags = self._graph.get_nodes(
-                TagNode, playlist['tags'], ignore_none=True)
+            songs = self._graph.get_nodes(SongNode, playlist['songs'])
+            tags = self._graph.get_nodes(TagNode, playlist['tags'])
 
             for song in songs:
                 song_counts[song] += 1
@@ -40,8 +39,21 @@ class MostPopular(BaseModel):
         return self
 
     def predict(self, question):
+        songs = question['songs']
+        tags = question['tags']
+
+        predicted_songs = self._most_popular_songs
+        predicted_tags = self._most_popular_tags
+
+        filtered_songs = remove_seen(
+            predicted_songs[:self._max_songs + len(songs)],
+            songs)[:self._max_songs]
+        filtered_tags = remove_seen(
+            predicted_tags[:self._max_tags + len(tags)],
+            tags)[:self._max_tags]
+
         return {
             'id': question['id'],
-            'songs': self._most_popular_songs,
-            'tags': self._most_popular_tags,
+            'songs': filtered_songs,
+            'tags': filtered_tags,
         }
