04386434133de7adbcc63fbb88ee71f69a604ecb
HeoHeo <h0h6h2h5@gmail.com>
2020-07-27 23:58:56 +0900

    Update solve_multiprocess.py

--- cf/solve_multiprocess.py
+++ cf/solve_multiprocess.py
@@ -14,18 +14,6 @@ from arena_util import load_json
 NUM_CORE = 1
 
 
-# def write_json(data, fname):
-#     def _conv(o):
-#         if isinstance(o, (np.int64, np.int32)):
-#             return int(o)
-#         raise TypeError
-
-#     parent = os.path.dirname(fname)
-#     distutils.dir_util.mkpath(parent)
-#     with open(fname, 'w', encoding='utf-8') as f:
-#         json_str = json.dumps(data, ensure_ascii=True, default=_conv)
-#         f.write(json_str)
-
 def write_json(data, fname):
     def _conv(o):
         if isinstance(o, np.int64):
