acb89bc6e7590f783cf014c773c39ab7e6f5a447
h2-heo <h0h6h2h5@gmail.com>
2020-07-27 21:29:59 +0900

    Add options

--- README.md
+++ README.md
@@ -6,15 +6,56 @@
 
 ## 수행 방법
 
+### 기본 사용법
+
 인자로, song_meta.json의 위치, train.json의 위치, question파일(val.json, test.json)의 위치, 장르 파일 위치(genre_gn_all.json)을 넘겨줍니다.
-	
-	```bash
-	$> python run.py run \
-		--song_meta_fname=res/song_meta.json \
-		--train_fname=arena_data/orig/train.json \
-		--question_fname=arena_data/questions/val.json \
-		--genre_fname=res/genre_gn_all.json
-	```
+
+```bash
+$> python run.py run \
+	--song_meta_fname=res/song_meta.json \
+	--train_fname=arena_data/orig/train.json \
+	--question_fname=arena_data/questions/val.json \
+	--genre_fname=res/genre_gn_all.json
+```
+
+그 밖에 사용할 수 있는 옵션은 다음과 같습니다:
+  - `--jobs=6`:
+      	멀티프로세싱을 사용합니다.
+		메모리가 더 많이 요구되지만 시간이 단축됩니다.
+		윈도우 환경에서 그래프 모델은 멀티프로세싱을 사용할 수 없습니다.
+  - `--train_fname2=res/val.json --train_fname3=res/test.json`:
+        여러 train을 병합하여 학습데이터로 사용합니다.
+		두 파일이 모두 명시되야 작동하며, 그래프 모델에만 적용됩니다.
+
+### 공개 리더보드용 문제 예측 예시
+
+다음 명령어를 입력합니다:
+
+```bash
+$> python run.py run \
+	--song_meta_fname=res/song_meta.json \
+	--train_fname=res/train.json \
+	--train_fname2=res/val.json \
+	--train_fname3=res/test.json \
+	--question_fname=res/val.json \
+	--genre_fname=res/genre_gn_all.json \
+	--jobs=6
+```
+
+### 파이널 리더보드용 문제 예측 예시
+
+다음 명령어를 입력합니다:
+
+```bash
+$> python run.py run \
+	--song_meta_fname=res/song_meta.json \
+	--train_fname=res/train.json \
+	--train_fname2=res/val.json \
+	--train_fname3=res/test.json \
+	--question_fname=res/test.json \
+	--genre_fname=res/genre_gn_all.json \
+	--jobs=6
+```
 
 
 ## 실행 결과	
--- run.py
+++ run.py
@@ -1,4 +1,5 @@
 import fire
+import os
 import sys
 sys.path.append('./graph')
 sys.path.append('./cf')
@@ -6,29 +7,52 @@ sys.path.append('./cf')
 from ensembler import Ensembler
 from cf.solve_multiprocess import MultiprocessSolver
 from graph.grape_recommender import GrapeRecommender
+from arena_util import load_json
 from arena_util import write_json
 
 
+MERGED_TRAIN_FNAME = './arena_data/merged_train.json'
+RUNNING_IN_WINDOWS = os.name == 'nt'
+
+
 class Recommend:
-    def run(self, song_meta_fname, train_fname, question_fname, genre_fname):
+    def run(self, song_meta_fname, train_fname, question_fname, genre_fname, train_fname2=None, train_fname3=None, jobs=1):
+        wanna_use_merged_train = (train_fname2 is not None) and (train_fname3 is not None)
+        if wanna_use_merged_train:
+            self.merge_trains([train_fname, train_fname2, train_fname3], MERGED_TRAIN_FNAME)
+
+        graph_train_fname = MERGED_TRAIN_FNAME if wanna_use_merged_train else train_fname
+        graph_jobs = 1 if RUNNING_IN_WINDOWS else jobs
+
+        if RUNNING_IN_WINDOWS and jobs > 1:
+            print("[Warning] 그래프 추천은 윈도우 환경에서 멀티프로세싱이 불가능 합니다.")
+
         cf_solver = MultiprocessSolver()
         cf_solver.run(song_meta_fname=song_meta_fname,
                       train_fname=train_fname,
                       question_fname=question_fname,
-                      jobs=6)
+                      jobs=jobs)
 
         graph_solver = GrapeRecommender()
         graph_solver.run(song_meta_fname=song_meta_fname,
-                         train_fname=train_fname,
+                         train_fname=graph_train_fname,
                          question_fname=question_fname,
                          genre_fname=genre_fname,
-                         jobs=6
+                         jobs=jobs
                          )
 
         ensembler = Ensembler(['./graph/results/results.json', './cf/results/results.json'], question_fname)
         res = ensembler.ensemble()
         print(res[0])
         write_json(res, './results/results.json')
+
+    def merge_trains(self, train_fnames, output_fname):
+        merged_train = []
+        for train_fname in train_fnames:
+            merged_train += load_json(train_fname)
+
+        write_json(merged_train, output_fname)
+
 
 if __name__ == "__main__":
     fire.Fire(Recommend)
